94931038a2b8bea77ed6c9a5ced65ee241848daa
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-06-28 01:42:12 +0100

    Updated unit tests

--- backend/services/tests.py
+++ backend/services/tests.py
@@ -1,3 +1,15 @@
-# from django.test import TestCase
+from django.test import TestCase
+from .models import Service
 
-# Create your tests here.
+
+class ServiceModelUnitTestCase(TestCase):
+  def setUp(self):
+    self.service = Service.objects.create(
+      title='Service 1',
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt quis odio id.',
+      icon='settings'
+    )
+
+  def test_service_model(self):
+    data = self.service
+    self.assertIsInstance(data, Service)
