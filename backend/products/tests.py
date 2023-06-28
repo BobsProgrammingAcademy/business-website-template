f41466b3aa4a380d51d9a9c479b088c320cb3fe7
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-06-28 01:40:51 +0100

    Updated unit tests

--- backend/products/tests.py
+++ backend/products/tests.py
@@ -1,3 +1,14 @@
-# from django.test import TestCase
+from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+
+class ProductModelUnitTestCase(TestCase):
+  def setUp(self):
+    self.product = Product.objects.create(
+      name='Lorem ipsum dolor sit amet',
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt quis odio id.'
+    )
+
+  def test_product_model(self):
+    data = self.product
+    self.assertIsInstance(data, Product)
