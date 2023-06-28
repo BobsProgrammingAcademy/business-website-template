199ec5edc921d01c549595ddff4aa29d5fa848d6
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-06-28 01:35:49 +0100

    Updated unit tests

--- backend/contact/tests.py
+++ backend/contact/tests.py
@@ -1,3 +1,17 @@
-# from django.test import TestCase
+from django.test import TestCase
+from .models import Contact
 
-# Create your tests here.
+
+class ContactModelUnitTestCase(TestCase):
+  def setUp(self):
+    self.contact = Contact.objects.create(
+      address='Dublin, Ireland',
+      email='bob@test.com',
+      phone='123456789',
+      latitude='53.350140',
+      longitude='-6.266155'
+    )
+
+  def test_contact_model(self):
+    data = self.contact
+    self.assertIsInstance(data, Contact)
