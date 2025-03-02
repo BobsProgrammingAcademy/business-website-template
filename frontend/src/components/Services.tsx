34e5a5d171c9b8cea5425565135b4ed0e2b8d4ba
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2025-03-02 19:35:01 +0100

    Code refactoring

--- frontend/src/components/Services.tsx
+++ frontend/src/components/Services.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+
+// Material UI
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
