a3410c154c501f0d20b42f7357243c25fbf6e4e9
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2025-10-22 20:13:44 +0200

    Code refactoring

--- frontend/src/tests/Home.test.tsx
+++ frontend/src/tests/Home.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+// Components
 import Home from '../pages/Home';
 
 const renderApp = () => {
